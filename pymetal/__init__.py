fa5fdbbe25db2b3daa4e94d915c25a9384a61732
jarbasai <jarbasai@mailfence.com>
2018-03-22 06:22:46 +0000

    pep8

--- pymetal/__init__.py
+++ pymetal/__init__.py
@@ -72,9 +72,13 @@ class MetalArchives(object):
         url = self.site_url + self.url_search_songs + urlencode(params)
         songs = json.load(urlopen(url))['aaData']
         for song in songs:
-            data = {"album_url": song[0][song[0].find('href="') + 5:song[0].find('" title=')],
-                    "band_name": song[0][song[0].find('>') + 1:song[0].find('</a')],
-                    "album_name": song[1][song[1].find('">') + 2:song[1].find('</a')],
+            data = {"album_url": song[0][
+                                 song[0].find('href="') + 5:song[0].find(
+                                     '" title=')],
+                    "band_name": song[0][
+                                 song[0].find('>') + 1:song[0].find('</a')],
+                    "album_name": song[1][
+                                  song[1].find('">') + 2:song[1].find('</a')],
                     "album_type": song[2],
                     "song_name": song[3],
                     "song_id": self.lyric_id_re.search(song[4]).group("id")}
@@ -83,29 +87,20 @@ class MetalArchives(object):
     def search_band(self, band_name="", genre="", start=0):
         params = dict(bandName=band_name, genre=genre, iDisplayStart=start)
         url = self.site_url + self.url_search_bands + urlencode(params)
-        bands, num = json.load(urlopen(url))['aaData'], json.load(urlopen(url))["iTotalRecords"]
+        bands, num = json.load(urlopen(url))['aaData'], \
+                     json.load(urlopen(url))["iTotalRecords"]
         for band in bands:
-            data = {"url": band[0][band[0].find('href="') + 5:band[0].find('">')],
-                    "name": band[0][band[0].find('">') + 2:band[0].find('</a>')],
-                    "genre": band[1],
-                    "country": band[2]}
+            data = {
+                "url": band[0][band[0].find('href="') + 5:band[0].find('">')],
+                "name": band[0][band[0].find('">') + 2:band[0].find('</a>')],
+                "genre": band[1],
+                "country": band[2]}
             yield data
 
     def get_lyrics(self, song_id):
         url = self.site_url + self.url_lyrics + song_id
-        lyrics = self.tags_re.sub('', urlopen(url).read().strip()).decode('utf-8')
+        lyrics = self.tags_re.sub('', urlopen(url).read().strip()).decode(
+            'utf-8')
         if lyrics == self.lyrics_not_available:
             return None
         return lyrics
-
-
-m = MetalArchives()
-print m.random_band()
-print m.get_band_data("https://www.metal-archives.com/bands/Burzum/88")
-for band in m.search_band("metallica"):
-    print band
-for band in m.search_band(genre="black metal"):
-    print band
-for song in m.search_song(band_name="metallica"):
-    print song
-print m.get_lyrics("67900")
