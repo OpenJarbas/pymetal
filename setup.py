8cc50a97de22c53dda6d0cd90d861164963a930a
jarbasal <jarbasai@mailfence.com>
2019-12-13 03:03:36 +0000

    0.4.1

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='pymetal',
-    version='0.4',
+    version='0.4.1',
     packages=[],
     install_requires=["requests", "bs4", "lxml", "random-user-agent"],
-    url='https://www.github.com/jarbasal/pymetal',
-    license='MIT',
+    url='https://www.github.com/OpenJarbas/pymetal',
+    license='Apache2.0',
     author='jarbasAi',
     author_email='jarbasai@mailfence.com',
     description='metal archives, dark lyrics api'
